fdcc1d93e8e63da16644bc3545c25d4c190b2ee1
Juraj Borza <jborza@gmail.com>
2025-05-06 19:18:35 +0200

    count parameter also used for search

--- src/app/search-external/search-external.component.ts
+++ src/app/search-external/search-external.component.ts
@@ -25,14 +25,16 @@ export class SearchExternalComponent {
         this.route.queryParamMap.subscribe((params) => {
           this.searchQuery = params.get('search_query') || '';
           this.where = params.get('where') || 'openlibrary';
+          this.count = parseInt(params.get('count') || '10', 10);
           if (this.searchQuery) {
-            this.fetchSearchResults(this.where, this.searchQuery);
+            this.fetchSearchResults();
           }
         });
       }
 
-      fetchSearchResults(where: string, query: string): void {
-        this.searchService.searchBooks(where, query, this.count).subscribe((response) => {
+      fetchSearchResults(): void {
+        this.searchService.searchBooks(this.where, this.searchQuery, this.count)
+        .subscribe((response) => {
           this.books = response;
         });
       }
