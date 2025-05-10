3a30eb39b7de7cc3533439dffae6fca4b49526a1
Juraj Borza <jborza@gmail.com>
2025-05-10 20:32:35 +0200

    prepared import form

--- src/app/import-page/import-page.component.ts
+++ src/app/import-page/import-page.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-import-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './import-page.component.html',
   styleUrl: './import-page.component.less'
 })
 export class ImportPageComponent {
-  uploadFile(event: any): void {
-    // TODO: Handle file import and submit data to your API
+
+  importCsvForm = new FormGroup({
+    csvFile: new FormControl('')
+  });
+  importNotesForm = new FormGroup({
+    titleAuthor: new FormControl(''),
+    authorTitle: new FormControl(''),
+    notes: new FormControl('')
+  });
+
+  ngOnInit() {
+    this.importNotesForm.controls.authorTitle.setValue("1");
+  }
+
+  onSubmitCsv() {
+    throw new Error('Method not implemented.');
+  }
+  onSubmitNotes() {
+    throw new Error('Method not implemented.');
   }
 }
