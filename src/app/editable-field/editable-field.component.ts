142bd0c3b8302a4e607d977ab93ad5c72abca1f9
Juraj Borza <jborza@gmail.com>
2025-05-10 10:26:01 +0200

    added multiline option for editable field (book description)

--- src/app/editable-field/editable-field.component.ts
+++ src/app/editable-field/editable-field.component.ts
@@ -11,9 +11,15 @@ import { CommonModule } from '@angular/common';
 export class EditableFieldComponent {
   @Input() currentValue!: string | undefined; // The value being edited
   @Output() currentValueChange = new EventEmitter<string>(); // Output to emit changes to the parent
+
   @Input() originalValue!: string | undefined; // The original value for comparison
   @Input() label!: string; // The label text
   @Input() controlId!: string; // Unique ID for input and checkbox
+
+  @Input() isChecked: boolean = false; // Input for checkbox state
+  @Output() isCheckedChange = new EventEmitter<boolean>(); // Output for checkbox state changes
+
+  @Input() multiline: boolean = false;
 
   // Method to emit the updated value when the input changes
   onInputChange(value: string): void {
@@ -26,4 +32,17 @@ export class EditableFieldComponent {
       this.onInputChange(inputElement.value); // Emit the value
     }
   }
+
+  onCheckboxChange(event: Event): void {
+    // Safely cast EventTarget to HTMLInputElement
+    const inputElement = event.target as HTMLInputElement;
+    this.isChecked = inputElement.checked;
+    this.isCheckedChange.emit(this.isChecked);
+    console.log('onCheckboxChange - '+this.label+" is " + this.isChecked);
+  }
+
+  // onCheckboxChange(checked: boolean): void {
+  //   this.isChecked = checked;
+  //   this.isCheckedChange.emit(this.isChecked);
+  // }
 }
