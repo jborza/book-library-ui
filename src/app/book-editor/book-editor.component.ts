cad49f02fef2800b49fb131c1ddd34d246a3a071
Juraj Borza <jborza@gmail.com>
2025-05-10 10:26:58 +0200

    observable for save book

--- src/app/book-editor/book-editor.component.ts
+++ src/app/book-editor/book-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BookDataService } from '../book-data.service';
@@ -6,6 +6,7 @@ import { Book } from '../book.model';
 import { FormsModule } from '@angular/forms';
 import { BooksService } from '../books.service';
 import { EditableFieldComponent } from '../editable-field/editable-field.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-editor',
@@ -18,10 +19,14 @@ export class BookEditorComponent implements OnInit {
   book!: Book;
   originalBook!: Book;
   searchBook!: any; // should it be also Book?
+  selectAllChecked: boolean = false;
+
+  @ViewChildren(EditableFieldComponent) editableFields!: QueryList<EditableFieldComponent>;
 
   constructor(private route: ActivatedRoute,
     private bookDataService: BookDataService,
     private booksService: BooksService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -56,22 +61,31 @@ export class BookEditorComponent implements OnInit {
     this.book[field] = value; // Dynamically update the field
   }
 
-  //TODO handle select all click/unclick
 
   saveBook(): void {
-    console.log('Saving book:', this.book);
-    console.log('Book data before save:', JSON.stringify(this.book, null, 2));
     // POST request to save the book
-    //this.booksService.saveBook(this.book).subscribe( ) .... ?
     // book/BOOK_ID/edit_api, POST the data
-    // TODO replace with observer!!!
-    // TODO seems like we're saving the OLD book, not the new one - WTF
-    this.booksService.saveBook(this.book).subscribe((response) => {
-      console.log('Book saved successfully:', response);
-      alert('Book saved successfully!');
-      // Optionally, navigate back to the book details page or show a success message
-    }, (error) => {
-      console.error('Error saving book:', error);
+    this.booksService.saveBook(this.book)
+    .subscribe({
+      next: response => {
+        console.log('Book saved successfully:', response);
+      },
+      error: error => {
+        console.error('Error occurred:', error);
+      },
+      complete: () => {
+          // navigate back to the book details page
+          this.router.navigate(['/books', this.book.id]);
+      }
     });
+  }
+
+  toggleSelectAll(): void {
+    this.selectAllChecked = !this.selectAllChecked;
+    this.editableFields.forEach(field => field.isChecked = this.selectAllChecked);
+  }
+
+  updateSelectAllStatus(): void {
+    this.selectAllChecked = this.editableFields.toArray().every(field => field.isChecked);
   }
 }
