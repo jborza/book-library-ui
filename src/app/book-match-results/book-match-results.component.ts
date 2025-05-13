d33a63a801edf1f961ed26ed4fd7a7ff401917eb
Juraj Borza <jborza@gmail.com>
2025-05-13 15:13:34 +0200

    wip - match results - search form

--- src/app/book-match-results/book-match-results.component.ts
+++ src/app/book-match-results/book-match-results.component.ts
@@ -5,10 +5,13 @@ import { SearchService } from '../search.service';
 import { MatchResultItemComponent } from '../match-result-item/match-result-item.component';
 import { BookDataService } from '../book-data.service';
 import { levenshteinDistance } from '../string-utils';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-match-results',
-  imports: [CommonModule, MatchResultItemComponent],
+  imports: [CommonModule,
+    MatchResultItemComponent,
+    ReactiveFormsModule],
   templateUrl: './book-match-results.component.html',
   styleUrl: './book-match-results.component.less'
 })
@@ -16,13 +19,21 @@ export class BookMatchResultsComponent {
   searchQuery: string = '';
   bookId!: number;
   searchResults: any[] = [];
+  searchForm: FormGroup;
+  providers = ['Google Books', 'Open Library'];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private searchService: SearchService,
-    private bookDataService: BookDataService
-  ) {}
+    private bookDataService: BookDataService,
+    private fb: FormBuilder
+  ) {
+    this.searchForm = this.fb.group({
+      provider: [this.providers[0]], // Default to the first provider
+      searchTitle: '', // Default to the first search title
+    });
+  }
 
   ngOnInit(): void {
     // Get the search query from the route parameters
@@ -31,9 +42,14 @@ export class BookMatchResultsComponent {
       console.log('Book ID:', this.bookId);
     });
     this.route.queryParams.subscribe(params => {
-      this.searchQuery = params['query'] || '';
+      this.searchQuery = params['query'] || '';      
       this.performSearch();
     });
+
+  }
+
+  onSearch(): void {
+    console.log('Search Form Values:', this.searchForm.value);
   }
 
   sortByLevenshteinDistance(array: any[], title: string, author: string): any[] {
@@ -42,7 +58,7 @@ export class BookMatchResultsComponent {
         levenshteinDistance(a.title || '', title) + levenshteinDistance(a.author || '', author);
       const distanceB =
         levenshteinDistance(b.title || '', title) + levenshteinDistance(b.author || '', author);
-  
+
       return distanceA - distanceB;
     });
   }
