669e960246395433a7de9e08ea7a594808959b54
Juraj Borza <jborza@gmail.com>
2025-05-22 14:33:44 +0200

    icon picker component - #12

---
+++ src/app/icon-picker/icon-picker.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-icon-picker',
+  imports: [CommonModule],
+  templateUrl: './icon-picker.component.html',
+  styleUrl: './icon-picker.component.less'
+})
+export class IconPickerComponent {
+  icons: string[] = ['🌍', '🏔', '🏝','🏛','🏟','🏢','💒','🚌','✈','⌚','⚽','🕹','🎮','🎭','💎','🔊','🎧',
+    '📱','📺','🔍','💰','📈','📊','✂','⚔','🔬','🛒','✔','❌','💤','👾',
+    '📔','📕','📖','📗','📘','📙','📚','📓','📒','📃','📜','📄','📰','🗞','📑','🔖','🏷',
+  ];
+
+  @Output() iconSelected = new EventEmitter<any>();
+
+  constructor() {
+
+  }
+
+  selectedIcon: string = '';
+
+  selectIcon(icon: string): void {
+    this.selectedIcon = icon;
+    this.iconSelected.emit(this.selectedIcon);
+  }
+
+  onCancel():void{
+    
+  }
+
+  onSubmit(): void{
+    this.iconSelected.emit(this.selectedIcon);
+  }
+}
