7aef0c2e4583f37eb8406aeaa313591fa0991deb
Juraj Borza <jborza@gmail.com>
2025-05-25 10:59:53 +0200

    Changed scrolling behavior for books page - closes #27

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet, Scroll } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { MenuService } from './menu.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -15,7 +16,33 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'book-library-ui';
 
-  constructor(private menuService: MenuService) {}
+  constructor(private menuService: MenuService,
+    private router: Router, 
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof Scroll || event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        if (event instanceof Scroll) {
+          // Check if the current route is BookListComponent
+          if (event.routerEvent.url.includes('/books')) {
+            // Do nothing to disable scrolling
+            return;
+          }
+
+          // Default scroll behavior for other pages
+          if (event.position) {
+            // Scroll to the previous position
+            this.viewportScroller.scrollToPosition(event.position);
+          } else {
+            // Scroll to the top for new navigation
+            this.viewportScroller.scrollToPosition([0, 0]);
+          }
+        }
+      });
+    }
 
   get menuClass(){
     return this.menuService.isMenuOpen ? 'col-md-3' : 'col-auto';
