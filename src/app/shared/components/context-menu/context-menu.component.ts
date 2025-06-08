2c70ca79a7af9f60e23bec8d10116e24deb293ca
Juraj Borza <jborza@gmail.com>
2025-06-08 19:22:29 +0200

    context menu positioning itself so it doesn't go off screen - #31

--- src/app/shared/components/context-menu/context-menu.component.ts
+++ src/app/shared/components/context-menu/context-menu.component.ts
@@ -12,6 +12,7 @@ export class ContextMenuComponent {
   @Output() action = new EventEmitter<string>(); // Emit actions like 'markAsFinished'
   @ViewChild('menu', { static: false }) menu!: ElementRef<HTMLDivElement>;
   menuPosition = { top: '0px', left: '0px' };
+  isMenuOpen = false;
 
   onAction(action: string): void {
     this.action.emit(action); // Notify parent component of the selected action
@@ -21,7 +22,11 @@ export class ContextMenuComponent {
 
   openMenu(event: MouseEvent): void {
     event.preventDefault();
+    this.isMenuOpen = true;
+    setTimeout(() => this.positionMenu(event));
+  }
 
+  positionMenu(event: MouseEvent): void {
     // Calculate the desired position
     const menuWidth = this.menu.nativeElement.offsetWidth;
     const menuHeight = this.menu.nativeElement.offsetHeight;
@@ -34,11 +39,11 @@ export class ContextMenuComponent {
 
     // Adjust if the menu overflows the viewport
     if (left + menuWidth > viewportWidth) {
-      left = viewportWidth - menuWidth - 10; // Add some padding
+      left = viewportWidth - menuWidth - 20; // Add some padding
     }
 
     if (top + menuHeight > viewportHeight) {
-      top = viewportHeight - menuHeight - 10; // Add some padding
+      top = viewportHeight - menuHeight - 20; // Add some padding
     }
 
     this.menuPosition = { top: `${top}px`, left: `${left}px` };
