0ef82cab5579fb7c034f1b7274a7633438ba5ac8
Juraj Borza <jborza@gmail.com>
2025-06-22 22:13:04 +0200

    grid view titles can be shown/hidden with a top bar button - #8

--- src/app/features/books/services/book-list.service.ts
+++ src/app/features/books/services/book-list.service.ts
@@ -4,10 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class BookListService {
+  // TODO save this in settings
   gridItemSize: number = 200;
   readonly gridSizeIncrement: number = 20;
-  // TODO save this in settings
   viewMode: 'grid' | 'table' = 'grid';
+  showTitles: boolean = true;
 
   constructor() {}
 
@@ -15,8 +16,13 @@ export class BookListService {
     this.viewMode = mode;
   }
 
+  public get gridMinHeight(): number {
+    if (this.showTitles) return this.gridItemSize + 60;
+    else return this.gridItemSize+20;
+  }
+
   decrement() {
-  if (this.gridItemSize > 60) {
+    if (this.gridItemSize > 60) {
       this.gridItemSize -= this.gridSizeIncrement;
     }
   }
@@ -25,5 +31,9 @@ export class BookListService {
     if (this.gridItemSize < 300) {
       this.gridItemSize += this.gridSizeIncrement;
     }
+  }
+
+  toggleShowTitles() {
+    this.showTitles = !this.showTitles;
   }
 }
